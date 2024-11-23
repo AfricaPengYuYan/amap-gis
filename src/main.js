bbdeb9498746044e13c4760ff7b32670353ae673
HnPengYuYan <2731800406@qq.com>
2024-11-23 20:40:57 +0800

    init

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import * as Cesium from 'cesium';
-Cesium.Ion.defaultAccessToken ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4YjI3NjRmMy1jYjUwLTQ3ODQtOGFjZi01NDQzOWU3NjM4NDIiLCJpZCI6MjU3NDU2LCJpYXQiOjE3MzIzNjQ2MDB9.1fW-WfFZkxf8talOXbcuuYcWTh-ptjuwUyGrmE1t8e8'
-
-createApp(App).mount('#app')
