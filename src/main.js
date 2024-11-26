6803eb092ff12061e52dd037d7cb857b76b9c10d
HnPengYuYan <2731800406@qq.com>
2024-11-26 22:15:52 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
 import * as Cesium from 'cesium';
-Cesium.Ion.defaultAccessToken ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4YjI3NjRmMy1jYjUwLTQ3ODQtOGFjZi01NDQzOWU3NjM4NDIiLCJpZCI6MjU3NDU2LCJpYXQiOjE3MzIzNjQ2MDB9.1fW-WfFZkxf8talOXbcuuYcWTh-ptjuwUyGrmE1t8e8'
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4YjI3NjRmMy1jYjUwLTQ3ODQtOGFjZi01NDQzOWU3NjM4NDIiLCJpZCI6MjU3NDU2LCJpYXQiOjE3MzIzNjQ2MDB9.1fW-WfFZkxf8talOXbcuuYcWTh-ptjuwUyGrmE1t8e8';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
---
+++ src/views/index.vue
@@ -0,0 +1,12 @@
+<!-- 首页 -->
+<script setup lang="ts">
+import Cesium from '@/components/Cesium.vue';
+</script>
+
+<template>
+    <div class="">
+        <Cesium />
+    </div>
+</template>
+
+<style scoped></style>
